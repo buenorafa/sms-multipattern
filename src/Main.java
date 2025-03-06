9967fdfc72397e3816d8e0b7359c089f4d79d919
buenorafa <rafaelbuenooo@outlook.com>
2025-03-06 17:10:04 -0300

    Adiciona configuração inicial do projeto

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import api.SMSSender;
+import api.TimService;
+import model.SMS;
+
+public class Main {
+    public static void main(String[] args) {
+        SMSSender sender = new TimService();
+        SMS message = new SMS("83988885544","83988221133","Bom dia. Seu boleto ja esta disponivel para pagamento");
+        sender.sendSMS(message);
+    }
+}
