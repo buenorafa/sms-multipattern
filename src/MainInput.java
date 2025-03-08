54b71ef733aa9a251eeb400397c89dbaa3af5896
Sammuel13 <sammluna13@gmail.com>
2025-03-08 18:34:09 -0300

    adicionado suporte à inserir arquivo com numeros de entrada

--- src/MainInput.java
+++ src/MainInput.java
@@ -3,9 +3,20 @@ import api.SMSServiceTranslateDecorator;
 import api.TranslationService;
 import api.WhatsMateTranslationService;
 import model.SMS;
+import util.Util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainInput {
     public static void main(String[] args) {
+
+        List<String> destinos = new ArrayList<>();
+
+        if (args.length != 0) {
+            destinos = Util.readLinesFromFile(args[0]);
+        }
 
         SMSService smsService = new SMSService();
 
@@ -14,6 +25,28 @@ public class MainInput {
 
         String origem = "1583986762683";
         String destino;
+
+        if (!destinos.isEmpty()) {
+
+            System.out.println("Destinos selecionados: ");
+            for (String d : destinos) {
+                System.out.println(d);
+            }
+
+            System.out.println("Digite o texto da mensagem: ");
+            String texto = System.console().readLine();
+
+            for (String d : destinos) {
+                SMS sms = new SMS(origem, d, texto);
+                try {
+                    smsService.sendMessage(sms);
+                    smsServiceTranslate.sendMessage(sms);
+                } catch (Exception e) {
+                    System.out.println("Erro ao enviar mensagem para o número" + d + ": " + e.getMessage());
+                }
+            }
+            return;
+        }
 
         do {
             System.out.println("Digite o número de destino: ");
@@ -28,7 +61,7 @@ public class MainInput {
                 smsService.sendMessage(sms);
                 smsServiceTranslate.sendMessage(sms);
             } catch (Exception e) {
-                System.out.println("Erro ao enviar mensagem: " + e.getMessage());
+                System.out.println("Erro ao enviar mensagem para o número" + destino + ": " + e.getMessage());
             }
         } while (!destino.isEmpty());
 
--- src/util/Util.java
+++ src/util/Util.java
@@ -1,5 +1,11 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Util {
 
     public static String getAdapterType(String phoneNumber) {
@@ -26,5 +32,21 @@ public class Util {
 
     public static boolean isValidPhoneNumber(String destino) {
         return destino != null && destino.length() == 13 && isNumeric(destino);
+    }
+
+    public static List<String> readLinesFromFile(String filePath) {
+        List<String> lines = new ArrayList<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Erro ao ler arquivo de destinos: " + e.getMessage());
+        }
+        return lines;
     }
 }
